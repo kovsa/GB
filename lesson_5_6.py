4d249127fbdb84d75e10b21d565e6523a7d8118c
kovsa <sergeyakovalev@yandex.ru>
2020-10-22 14:32:51 +0300

    починил вар 2

--- lesson_5_6.py
+++ lesson_5_6.py
@@ -22,13 +22,10 @@ dict_all = dict(zip(dict_1, dict_2))
 print(dict_all)
 # ВАРИАНТ2 -------- С одним словарем---------------------
 print("\n ВАРИАНТ2 -------- С одним словарем---------------------\n")
-dict_3 = []
+dict_3 = {}
 with open("my_file_6.txt", "r") as file_6:
     for string in file_6:
         name_s = string.split()[0].strip()[0:-1]
         name_sum = sum(map(int, "".join([i for i in string if i == " " or i.isdigit()]).split()))
-        print(name_s)
-        print(name_sum)
         dict_3[name_s] = name_sum
 print(dict_3)
-
