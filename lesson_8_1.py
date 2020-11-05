bad82ca669ce2a37827471cb402a13e14d385b6a
kovsa <sergeyakovalev@yandex.ru>
2020-11-05 16:48:29 +0300

    починил

--- lesson_8_1.py
+++ lesson_8_1.py
@@ -7,36 +7,30 @@
 
 class Data:
     def __init__(self, day_month_year):
-        self.day_month_year = str(day_month_year)
+        self.day_month_year = day_month_year
 
     @classmethod
-    def extract(cls, day_month_year):
-        my_date = []
-        for i in day_month_year.split():
-            if i != "-":
-                my_date.append(i)
-                Data.valid(int(i))
-        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+    def extract(cls, date: "Data"):
+        return list(map(int, date.day_month_year.split("-")))
 
     @staticmethod
-    def valid(i):
-        if 1 <= i <= 31:
-            if 1 <= i <= 12:
-                if 1 <= i <= 9999:
-                    return f"ok"
-                else:
-                    print(f'Неправильный год')
-            else:
-                print(f"Неправильный месяц")
-        else:
-            print(f"Неправильный день")
+    def valid(date: "Data"):
+        parser = date.day_month_year.split("-")
+        if len(parser[2]) not in {2, 4}:
+            raise ValueError("Неправильный год")
+        if not 1 <= int(parser[1]) <= 12:
+            raise ValueError("Неправильный месяц")
+        if not 1 <= int(parser[0]) <= 31:
+            raise ValueError("Неправильный день")
+        print("Введенные данные ок")
 
     def __str__(self):
         return f"Текущая дата {Data.extract(self.day_month_year)}"
 
 
-today = Data("32 - 11 - 20001")
-print(today)
+date = Data("31-12-2020")
+date.valid(date)
+print(Data.extract(date))
 
 
 
